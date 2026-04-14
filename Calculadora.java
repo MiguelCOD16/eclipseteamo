e2c624f8570e6134156ec6ba96eadfef9792ef58
Miguel  Angel <gonzalezortegamiguelangel2005@gmail.com>
2026-04-14 13:29:08 +0200

    Update Calculadora.java to add division and multiplication methods

--- Calculadora.java
+++ Calculadora.java
@@ -1,3 +1,14 @@
 public class Calculadora {
     
+    public static double dividir(double dividendo, double divisor) {
+        if (divisor == 0) {
+            System.out.println("Error: No se puede dividir entre 0");
+            return 0;
+        }
+        return dividendo / divisor;
+    }
+    
+    public static double multiplicar(double numero1, double numero2) {
+        return numero1 * numero2;
+    }
 }
