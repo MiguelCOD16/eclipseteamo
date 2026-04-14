7dfd432b95b0d88986fd6939d2e62216e1e7e2e3
Miguel  Angel <gonzalezortegamiguelangel2005@gmail.com>
2026-04-14 14:16:45 +0200

    Refactor division method to use exception handling
    Updated division method to throw ArithmeticException for division by zero.

--- Calculadora.java
+++ Calculadora.java
@@ -11,14 +11,12 @@ public class Calculadora {
         return a - b;
     }
 
-    public static double dividir(double dividendo, double divisor) {
-        if (divisor == 0) {
-            System.out.println("Error: No se puede dividir entre 0");
-            return 0;
-        }
-        return dividendo / divisor;
+   public static double dividir(double dividendo, double divisor) {
+    if (divisor == 0) {
+        throw new ArithmeticException("No se puede dividir entre 0");
     }
-
+    return dividendo / divisor;
+}
     public static double multiplicar(double numero1, double numero2) {
         return numero1 * numero2;
     }
